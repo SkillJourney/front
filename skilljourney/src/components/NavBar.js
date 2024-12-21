b0f9193b1c79e5b6a9fef74809be249c9f59767a
yona-hammouchi <yona.hammouchi@laplateforme.io>
2024-12-22 00:19:46 +0100

    init home page, add button, input and search

--- skilljourney/src/components/NavBar.js
+++ skilljourney/src/components/NavBar.js
@@ -14,13 +14,13 @@ const NavBar = () => {
             <div className='nav_list'>
                 <ul>
                     <a onClick={() => navigate('/')}><li>HOME</li></a>
-                    <a onClick={() => navigate('/corporate')}><li>ENTREPRISES</li></a>
+                    <a onClick={() => navigate('/corporate')}><li>FOR EMPLOYEES</li></a>
                     <a onClick={() => navigate('/contact')}><li>CONTACT</li></a>
                 </ul>
 
                 <div className='containers_butt'>
-                    <a onClick={() => navigate('/register')}><Button>S'INSCRIRE</Button></a>
-                    <a onClick={() => navigate('/login')}><Button>CONNEXION</Button></a>
+                    <a onClick={() => navigate('/register')}><Button>SIGN UP</Button></a>
+                    <a onClick={() => navigate('/login')}><Button>LOGIN</Button></a>
                 </div>
             </div>
 
--- skilljourney/src/pages/HomePage.js
+++ skilljourney/src/pages/HomePage.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import backg from '../assets/backs.jpg'
+import { Input, Button, } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 
 const HomePage = () => {
+
+
+
     return (
-        <div className='containers_back'>
-            <img src={backg} />
-        </div>
+
+        <div className='containers_home'>
+            <div className='containers_back'>
+                <div className='containers_info_job'>
+                    <div className='title'>
+                        <h2>Find your job</h2>
+                        <div className='search-bar'>
+                            <Input
+                                placeholder="Job title, keywords or company name"
+                            />
+                            <Input
+                                placeholder="city, province or région"
+                            />
+                            <Button>Rechercher</Button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div >
     );
 };
 
